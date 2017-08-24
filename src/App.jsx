f2e7d5b09cd962d2590e896c810e015105c3d8a9
Hanocha <saiki@feedforce.jp>
2017-08-24 16:58:19 +0900

    Get an id of own gist from github with apollo

--- src/App.jsx
+++ src/App.jsx
@@ -2,60 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {
-  QueryRenderer,
+  ApolloClient,
+  createNetworkInterface,
+  ApolloProvider,
+  gql,
   graphql,
-} from 'react-relay';
-import {
-  Environment,
-  Network,
-  RecordSource,
-  Store,
-} from 'relay-runtime';
+} from 'react-apollo';
 
 import List from './List';
 
 const mountNode = document.getElementById('root');
 
-function fetchQuery(
-  operation,
-  variables,
-) {
-  return fetch('https://api.github.com/graphql', {
-    method: 'POST',
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.github.com/graphql',
+  opts: {
     headers: {
       'Content-Type': 'application/json',
-      'Authorization: bearer': process.env.TOKEN
+      'Authorization': `bearer ${process.env.TOKEN}`,
     },
-    body: JSON.stringify({
-      query: operation.text,
-      variables,
-    }),
-  }).then(response => {
-    return response.json();
-  });
-}
+  },
+})
 
-const modernEnvironment = new Environment({
-  network: Network.create(fetchQuery),
-  store: new Store(new RecordSource()),
+const client = new ApolloClient({
+  networkInterface: networkInterface,
 });
 
+const listQuery = gql`
+query {
+  viewer {
+    gists(first: 1) {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+}
+`;
+
+const ListWithData = graphql(listQuery)(List);
+
 ReactDOM.render(
-  <QueryRenderer
-    environment={modernEnvironment}
-    query={graphql`
-      query AppQuery {
-        gists
-      }
-    `}
-    variables={{}}
-    render={({error, props}) => {
-      if (props) {
-        return <List viewer={props.viewer} />;
-      } else {
-        return <div>Loading</div>;
-      }
-    }}
-  />,
+  <ApolloProvider client={client}>
+    <ListWithData />
+  </ApolloProvider>,
   mountNode
 );
--- src/List.jsx
+++ src/List.jsx
@@ -1,1 +1,9 @@
-export default () => <div>Hello!</div>
+import React from 'react';
+
+export default ({ data: { viewer } }) => {
+  if (viewer) {
+    return <div>{viewer.gists.edges[0].node.id}</div>;
+  }
+
+  return <div>loading...</div>;
+}

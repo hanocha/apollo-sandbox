d34600c7a172d50e6d8f3212960b397373829c2a
Hanocha <saiki@feedforce.jp>
2017-09-05 12:00:44 +0900

    Add onClick event handler on Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const Button = ({ text }) => (
-  <button>{text}</button>
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
 );
 
 export default Button;
